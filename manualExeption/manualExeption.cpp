c5ffd231f94c3eaab0219439388fbe295ee365c3
dianfitrip <dianfitri683@gmail.com>
2024-06-04 11:02:10 +0700

    exeption. try and catch

---
+++ manualExeption/manualExeption.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try {
+		cout << "Selamat belajar di prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuah integer maka
+		cout << "pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch(int a) {
+		//blok ini akan dieksekusi
+		cout << "pengecualian akan dieksekusi" << endl;
+	}
+
+	catch (...) {
+		/*jika selain interger maka blok ini akan dieksekusi*/
+		cout << "default pengecualian dieksekusi" << endl;
+	}
+	return 0;
+}
