0649894deb1ce9bb5b4de1679e55606daca0a304
dianfitrip <dianfitri683@gmail.com>
2024-06-04 10:20:13 +0700

    membuka dan menunjuk file

--- StreamandWriter/StreamandWriter.cpp
+++ StreamandWriter/StreamandWriter.cpp
@@ -7,4 +7,9 @@ int main() {
 	//untuk menyimpan data berbentuk string
 	string baris;
 
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
 	
